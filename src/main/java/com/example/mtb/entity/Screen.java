cb25a8a69e4c00389b7d024268b5d364889a49f2
manojbm12 <manojbmmanu77@gmail.com>
2025-04-30 09:21:12 +0530

    partial-aduting

--- src/main/java/com/example/mtb/entity/Screen.java
+++ src/main/java/com/example/mtb/entity/Screen.java
@@ -4,22 +4,51 @@ import com.example.mtb.enums.ScreenType;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Setter
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public class Screen {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "screen_id",nullable = false,updatable = false)
     private String screenId;
+
+    @Column(name = "screen_type",nullable = false,updatable = false)
     @Enumerated(EnumType.STRING)
     private ScreenType screenType;
+
+
+    @Column(name = "capacity",nullable = false,updatable = false)
     private int capacity;
+
+
+    @Column(name = "noOfRows",nullable = false,updatable = false)
     private int noOfRows;
+
+     @CreatedDate
+    @Column(name = "created_At",nullable = false,updatable = false)
     private Long createdAt;
+
+   @LastModifiedDate
+    @Column(name = "update_at",nullable = false,updatable = false)
     private Long updatedAt;
+
+    @CreatedBy
+    @Column(name = "create_by",nullable = false,updatable = false)
     private String createdBy;
+
+
+    @LastModifiedBy
+    @Column(name = "update_by",nullable = false)
+    private String updatedBy;
 
     @ManyToOne
     private Theater theater;
--- src/main/java/com/example/mtb/entity/Theater.java
+++ src/main/java/com/example/mtb/entity/Theater.java
@@ -3,24 +3,52 @@ package com.example.mtb.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.List;
 
 @Setter
 @Getter
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Theater {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "theater_id",nullable = false,updatable = false)
     private String theaterId;
+
+    @Column(name = "name",nullable = false)
     private String name;
+
+    @Column(name = "address",nullable = false)
     private String address;
+
+    @Column(name = "city",nullable = false)
     private String city;
+
+    @Column(name = "landmark",nullable = false)
     private String landmark;
+
+    @CreatedDate
+    @Column(name = "create_at",nullable = false,updatable = false)
     private Long createAt;
+
+    @LastModifiedDate
+    @Column(name = "update_at",nullable = false,updatable = false)
     private  Long updatedAt;
+
+    @CreatedBy
+    @Column(name = "create_by",nullable = false,updatable = false)
     private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "update_by",nullable = false)
+    private String updatedBy;
 
     @ManyToOne()
     private TheaterOwner theaterOwner;
